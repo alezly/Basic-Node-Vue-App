ae12448036b1ee759e54ce163d59ff718c13794b
alezly <albanyramirez8@gmail.com>
2024-03-07 22:33:08 -0400

    avances navbar

---
+++ src/components/adminLogin.vue
@@ -0,0 +1,28 @@
+<script>
+  export default {
+    data() {
+      return {
+        form: {
+          user: '',
+          password: '',
+        },
+      }
+    },
+    methods: {
+      login(){
+        alert(this.form)
+      }
+    }
+  }
+</script>
+<template>
+<div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" v-model="form.user" placeholder="Enter Username" name="uname" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" v-model="form.password" placeholder="Enter Password" name="psw" required>
+
+    <button type="submit" @click="login">Login</button>
+  </div>
+</template>
---
+++ src/components/navbar.vue
@@ -0,0 +1,58 @@
+<script>
+// import { RouterLink, RouterView } from 'vue-router'
+
+export default {
+  name: 'NavBar'
+}
+</script>
+<template>
+  <nav class="navbar">
+    <div class="navbar-logo">
+      <RouterLink class="navbar-item" to="/"><img src="../assets/logoColor.png" alt="Logo" /></RouterLink>
+    </div>
+    <ul class="navbar-items">
+      <RouterLink class="navbar-item" to="/">Inicio</RouterLink>
+      <RouterLink class="navbar-item" to="/Servicios">Servicios</RouterLink>
+      <RouterLink class="navbar-item" to="/QuienesSomos">Quiénes somos</RouterLink>
+      <RouterLink class="navbar-item" to="/Comunicate">Comunícate con nosotros</RouterLink>
+      <RouterLink class="navbar-item" to="/blog">Blog</RouterLink>
+    </ul>
+  </nav>
+  <RouterView />
+</template>
+
+<style scoped>
+.navbar {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 1rem 2rem;
+  background-color: white;
+  box-shadow: 0px 3px 6px #00000029;
+}
+.navbar-logo{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.navbar-logo img {
+  height: 1.2rem;
+  cursor: pointer;
+  margin-top: 0.2rem;
+}
+
+.navbar-items {
+  display: flex;
+  list-style-type: none;
+  padding: 0;
+}
+
+.navbar-item {
+  margin: 0 1rem;
+  cursor: pointer;
+  font-weight: 200;
+  text-decoration: none;
+  color: #464646;
+  font-family: "Outfit", sans-serif;
+}
+</style>
